7321a63cc6ebc7e680e433386957b3883596f5a4
Feernandaaa <nandapoderosa.mf@gmail.com>
2022-06-22 15:37:17 -0300

    finalizando projeto

--- js/game.js
+++ js/game.js
@@ -1,21 +1,25 @@
 // estou atribuindo o elemento com o #id saudacao , a constante saudacao(h3 que exibe o nome do jogador)
-const saudacao = document.querySelector('#saudacao');
-// estou atribuindo os elementos com classe botao-quiz , a constante botaoQuiz (button ver resposta)
-const botaoQuiz = document.querySelectorAll('.botao-quiz');
+const saudacao = document.querySelector('#saudacao')
+
 // estou acessando a propriedade de texto da constante saudacao e atribuindo o valor do sessionStorange(guarda dados de uma sessao em um navegador)
-saudacao.textContent = 'Olá ' + sessionStorage.getItem("name");
+
+saudacao.textContent = 'Olá, ' + sessionStorage.getItem('name')
 
 //percorrendo a array
-botaoQuiz.forEach((element, index) => {
-    element.addEventListener('click', function(){
-        const resposta = element.dataset.resposta;
-        const num = index + 1;
-        const label = document.querySelector('label[for=q'+ num +'_alt'+ resposta +']');
-        label.classList.add('active');
-        element.textContent = 'Alternativa Correta ' + resposta;
-    })
-});
 
+function verificar() {
+  if (!window.confirm('Você tem certeza que essas são as respostas?'))
+    return false
 
+  var resposta = document.getElementById('correta')
+  var erros = 0
+  var acertos = 0
+  return alert(
+    'ACERTOU - Número de acertos: ' +
+      acertos +
+    '. ERROU - Número de erros: ' +
+      erros
+  )
+}
 
 // manipula elementos das páginas de quiz.
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Definicão do Nome e Categorias
+
+- Nome do Quiz: Quiz Acerte se Puder!
+- Categorias: Filme (Avengers Ultimato) e Série (Stranger Thinges)
+
+## Desenho do 1° layout (Marco 1)
+
+.. layout1.png
+![Tux, the Linux mascot](/img/layout-1.jpg)
+
+### Desevolvimento
+
+Criado um arquivo index.html e o style.css, e realizamos o progresso do trivia, sem a interação do botão.
+
+## Desenho do 2° layout (Marco 2)
+![Tux, the Linux mascot](/img//layout%202.jpg)
+
+.. layout2.png
+
+### Desenvolvimento
+
+Foi alterado o primeiro arquivo index.html para filme-quiz.html e criado um novo arquivo index.html para ser nossa página principal, solicitando o nome do jogador. Aumentamos a quantidade de perguntas e fizemos a interação dos botões e do nome no main.js.
+
+## Desenho do 3° layout (Marco 3)
+![Tux, the Linux mascot](/img/layout-3.jpg)
+
+.. layout3.png
+
+### Desenvolvimento
+
+Criamos mais três arquivos sendo categoria.html e serie-quiz.html, e um novo arquivo game.js, pois estava dando erro nos botões e fizemos um arquivo separado.
+Retiramos todos botões que estavam por pergunta, e colocamos um unico botão para acionar todas as respostas, pensando da intereção do usuário.
